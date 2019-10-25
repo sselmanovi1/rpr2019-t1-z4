43a9fd602d17fd3d1760ab931dec035528efc52f
sselmanovi1 <esmaimubineta123>
2019-10-25 11:25:49 +0200

    sumka

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,61 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] artikliUKorpi = new Artikl[50];
+
+    public int getBrojArtikalaUKorpi() {
+        return brojArtikalaUKorpi;
+    }
+
+    public void setBrojArtikalaUKorpi(int brojArtikalaUKorpi) {
+        this.brojArtikalaUKorpi = brojArtikalaUKorpi;
+    }
+
+    private int brojArtikalaUKorpi;
+
+    public Artikl[] getArtikliUKorpi() {
+        return artikliUKorpi;
+    }
+
+    public void setArtikliUKorpi(Artikl[] artikliUKorpi) {
+        this.artikliUKorpi = artikliUKorpi;
+    }
+
+    public Korpa() {
+        this.brojArtikalaUKorpi = 0;
+    }
+
+    public boolean dodajArtikl(Artikl a) {
+        //ako je uspjesno dodao artikl vratit ce true, a u suprotnom false
+        if(this.brojArtikalaUKorpi == 50){
+            return false;
+        }
+        else {
+            this.artikliUKorpi[brojArtikalaUKorpi] = a;
+            brojArtikalaUKorpi++;
+        }
+        return true;
+    }
+
+    public Artikl[] getArtikli() {
+        return this.artikliUKorpi;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for(Artikl a : artikliUKorpi){
+            if(a.getKod().equals(kod)){
+                brojArtikalaUKorpi--;
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int cijena = 0;
+        for(Artikl a : artikliUKorpi)
+            cijena += a.getCijena();
+
+        return cijena;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,39 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] artikliUSupermarketu = new Artikl[1000];
+
+    public int getBrojArtikalaUSupermarketu() {
+        return brojArtikalaUSupermarketu;
+    }
+
+    private int brojArtikalaUSupermarketu;
+
+    public Supermarket() {
+        brojArtikalaUSupermarketu = 0;
+    }
+
+    public void dodajArtikl(Artikl a){
+        if(brojArtikalaUSupermarketu == 10000){
+            System.out.println("U supermarketu se nalazi maksimalan broj artikala.");
+        }
+        else {
+            artikliUSupermarketu[brojArtikalaUSupermarketu] = a;
+            brojArtikalaUSupermarketu++;
+        }
+    }
+
+    public Artikl[] getArtikli() {
+        return this.artikliUSupermarketu;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for(Artikl a : artikliUSupermarketu){
+            if(a.getKod().equals(kod)){
+                brojArtikalaUSupermarketu--;
+                return a;
+            }
+        }
+        return null;
+    }
 }
